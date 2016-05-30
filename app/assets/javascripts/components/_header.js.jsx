28cd176ddfda0270c6545f8e8173702a1c0a06d5
Kimiko Kano <kskano@gmail.com>
2016-05-30 13:16:20 -0600

    Add Header component

---
+++ app/assets/javascripts/components/_header.js.jsx
@@ -0,0 +1,11 @@
+class Header extends React.Component {
+  constructor() {
+    super();
+  }
+
+  render() {
+    return (
+      <h1>This is the Header!</h1>
+    )
+  }
+}
--- app/assets/javascripts/components/_main.js.jsx
+++ app/assets/javascripts/components/_main.js.jsx
@@ -6,7 +6,7 @@ class Main extends React.Component {
   render() {
     return (
       <div>
-        <h1>Hello, Creact!</h1>
+        <Header />
       </div>
     )
   }
