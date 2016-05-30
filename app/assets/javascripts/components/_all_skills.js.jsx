1c4d39d95bc3b692f616cb23157aa3b3305493fe
Kimiko Kano <kskano@gmail.com>
2016-05-30 14:08:31 -0600

    Add skills to page with ajax call

--- app/assets/javascripts/components/_all_skills.js.jsx
+++ app/assets/javascripts/components/_all_skills.js.jsx
@@ -1,11 +1,28 @@
 class AllSkills extends React.Component {
   constructor() {
     super();
+    this.state = { skills: [] };
+  }
+
+  componentDidMount() {
+    $.getJSON('/api/v1/skills.json', (response) => { this.setState({ skills: response }) });
   }
 
   render() {
+    let skills = this.state.skills.map((skill) => {
+      return (
+        <div key={skill.id}>
+          <h3>{skill.name}</h3>
+          <p><strong>Level:</strong> {skill.level}</p>
+          <p>{skill.details}</p>
+        </div>
+      )
+    });
+
     return (
-      <h1>All the skills</h1>
+      <div>
+        {skills}
+      </div>
     )
   }
 }
